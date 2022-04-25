d4ef623c964f5001c43b66ae643b12c1f25532e2
Calebbii <biicaleb316@gmail.com>
2022-04-25 08:23:55 +0300

    Updates Views function

---
+++ data/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import redirect, render
+
+from data.forms import CreateStudentForm,UpdateStudentForm
+from data.models import Student
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    student = Student.objects.all()
+    return render(request, 'home.html', {'student':student})
+
+
+def student(request, student_id):
+    student = Student.objects.all()
+    return render(request, 'new_student.html', {'student':student})
+
+
+
+def create_student(request):
+    if request.method == 'POST':
+        create_student_form = CreateStudentForm(request.POST, request.FILES)
+        if create_student_form.is_valid():
+            student = create_student_form.save(commit=False)
+            student.admin = request.user.profile
+            student.save()
+            return redirect('home')
+    else:
+        create_student_form = CreateStudentForm()
+    return render(request, 'new_student.html', {'create_student_form': create_student_form})
+
+def update_student(request, student_id):
+    student = Student.objects.get(pk=student_id)
+    if request.method == 'POST':
+        update_student_form = UpdateStudentForm(request.POST,request.FILES, instance=student)
+        if update_student_form.is_valid():
+            update_student_form.save()
+            messages.success(request, f'Post updated!')
+            return redirect('home')
+    else:
+        update_student_form = UpdateStudentForm(instance=student)
+
+    return render(request, 'update_student.html', {"update_student_form":update_student_form})
+
+
+def delete_student(request,student_id):
+    student = student.objects.get(pk=student_id)
+    if student:
+        student.delete_student()
+    return redirect('home')
